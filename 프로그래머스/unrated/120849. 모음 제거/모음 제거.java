581cfdc2c99d20accc3dfd335dbbc947f0fcb199
seyun31 <155538114+seyun31@users.noreply.github.com>
2024-01-11 00:49:45 +0900

    [level unrated] Title: 모음 제거, Time: 0.10 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string) {
+        
+        my_string = my_string.toLowerCase();
+        my_string = my_string.replaceAll("[aeiou]", "");
+        
+        return my_string;
+    }
+}
