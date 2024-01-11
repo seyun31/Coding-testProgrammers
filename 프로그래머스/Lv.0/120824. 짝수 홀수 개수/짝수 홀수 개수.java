40fdbe5e08a6182aa6487cddbe5fef9f823c6184
seyun31 <155538114+seyun31@users.noreply.github.com>
2024-01-11 20:19:17 +0900

    Update 짝수 홀수 개수.java

--- 프로그래머스/Lv.0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/Lv.0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -8,3 +8,15 @@ class Solution {
         return answer;
     }
 }
+/* 
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        for(int i = 0; i < num_list.length; i++)
+            answer[num_list[i] % 2]++;
+
+        return answer;
+    }
+}
+*/
