317a24db96bbe9bd35e071c833bebfdda8b5989f
seyun31 <155538114+seyun31@users.noreply.github.com>
2024-01-19 16:18:38 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int [2];
+        for(int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i+1]) {
+                answer[0] = array[i];
+                answer[1] = i;
+            } else {
+                answer[0] = array[i+1];
+                answer[1] = (i+1);
+            }
+        }
+        return answer;
+    }
+}
