748aa780be40acfaed21ad44325a3879be615db0
seyun31 <155538114+seyun31@users.noreply.github.com>
2024-01-19 16:09:11 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.03 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int max = numbers[0] * numbers[1];
+        for (int i = 0; i < numbers.length; i++){
+            for (int j = 0; j < numbers.length; j++) {
+                if (i == j) {
+                   continue;
+                } else if ((numbers[i] * numbers[j]) > max) {
+                     max = numbers[i] * numbers[j];
+                } else {
+                    continue;
+                }
+            }
+        }
+        return max;
+    }
+}
