8a712deb69eb98c5a90b824f98eab8254980daa3
seyun31 <155538114+seyun31@users.noreply.github.com>
2024-02-22 23:27:07 +0900

    [level 0] Title: n의 배수, Time: 0.01 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num, int n) {
+        if (num % n == 0) {
+            int answer = 1;
+            return answer;
+        } else {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
