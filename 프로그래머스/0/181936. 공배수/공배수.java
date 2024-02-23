4a1e093f8a5d42849f12675a4bafda3c1bb6bb16
seyun31 <155538114+seyun31@users.noreply.github.com>
2024-02-23 18:41:08 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        if (number % n == 0 && number % m == 0) {
+            int answer = 1;
+            return answer;
+        } else {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
