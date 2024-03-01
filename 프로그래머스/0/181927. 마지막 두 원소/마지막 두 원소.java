516f3aae759e5f992fac1c87f0a3aad2ee0d14c1
seyun31 <155538114+seyun31@users.noreply.github.com>
2024-03-01 23:31:40 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int n = num_list.length;
+        int[] answer = new int [n + 1];
+        for (int i = 0; i < n; i++) {
+            answer[i] = num_list[i];
+        }
+        answer[n] = num_list[n - 1] > num_list[n - 2] ? num_list[n - 1] - num_list[n - 2] : num_list[n - 1] * 2;
+        return answer;
+    }
+}
