ea043148f94cbeab8ad4e130439736cfec030bab
seyun31 <155538114+seyun31@users.noreply.github.com>
2024-07-02 14:03:28 +0900

    [level 1] Title: [1차] 비밀지도, Time: 6.84 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            String binary1 = String.format("%" + n + "s", Integer.toBinaryString(arr1[i])).replace(' ', '0');
+            String binary2 = String.format("%" + n + "s", Integer.toBinaryString(arr2[i])).replace(' ', '0');
+            
+            StringBuilder sb = new StringBuilder();
+            
+            for (int j = 0; j < n; j++) {
+                if (binary1.charAt(j) == '1' || binary2.charAt(j) == '1') {
+                    sb.append('#');
+                } else {
+                    sb.append(' ');
+                }
+            }
+            
+            answer[i] = sb.toString();
+        }
+        
+        return answer;
+    }
+}
